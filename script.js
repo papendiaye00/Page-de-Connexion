24d49fe3ce93df726e73a09d9514ba32024e6cae
Pape Mamadou NDIAYE <mn7939380@gmail.com>
2023-03-13 16:59:45 +0000

    page de contact et d'inscription avec le localStorage

---
+++ script.js
@@ -0,0 +1,40 @@
+const signUp = e => {
+    let fname = document.getElementById('fname').value,
+        lname = document.getElementById('lname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase() && 
+            data.lname.toLowerCase() == lname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ fname, lname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('fname').focus();
+        alert("Votre compte a été créer avec succes!!!!!.\n\n Veuillez-vous connecter.");
+    }
+    else{
+        alert("Ooppss Vous avez déjà un compte!!!\nVous vous etes déjà inscrit.");
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        location.href = "/";
+    }
+    e.preventDefault();
+}
